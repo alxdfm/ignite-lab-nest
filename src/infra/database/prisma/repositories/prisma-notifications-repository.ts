58c53105d6449cee2c256effbae16277fa31631a
Alexandre Machado <alexandre_fmachado@hotmail.com>
2023-01-16 21:24:59 -0300

    add paths to tsconfig and apply to directories

--- src/infra/database/prisma/repositories/prisma-notifications-repository.ts
+++ src/infra/database/prisma/repositories/prisma-notifications-repository.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { Notification } from '../../../../../src/application/entities/notification/notification';
-import { NotificationsRepository } from '../../../../../src/application/repository/notifications-repository';
+import { Notification } from '@application/entities/notification/notification';
+import { NotificationsRepository } from '@application/repository/notifications-repository';
 import { PrismaService } from '../prisma.service';
 
 @Injectable()
